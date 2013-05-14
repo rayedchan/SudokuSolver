82a63fedacfecda3efd442168cb5a48adcaf0b5f
rayedchan <rayedchan@hotmail.com>
2013-05-13 21:16:40 -0400

    add methods for sudoku row, column, and quadrant constraint checks

--- js/sudokuSolver.js
+++ js/sudokuSolver.js
@@ -1,3 +1,25 @@
+/* Sudoku Coordinate System 9x9 Board 
+ *  
+ *     A B C  D E F   G H I       
+ *   A      |       |
+ *   B  I   |  II   |  III
+ *   C      |       |
+ *    ------|-------|-------
+ *   D      |       |
+ *   E  IV  |   V   |  VI
+ *   F      |       |
+ *    ------|-------|-------
+ *   G      |       |
+ *   H  VII | VIII  |  IX
+ *   I      |       |
+ *
+ *   [Left - Right] = x coordinate  rows 
+ *   [Up - Down] = y coordinate columns
+ */
+
+
+var SUDOKU_BOARD_LENGTH = 9;
+
 //Load DOM and ready to be manipulated.
 $(document).ready(function()
 {
@@ -16,7 +38,7 @@ $(document).ready(function()
         var rowI = $('#rowI').val();
         var givenValues = new Array(rowA,rowB,rowC,rowD,rowE,rowF,rowG,rowH,rowI);
         //console.log(givenValues);
-               
+        
         //Validate user input data
         $.validateData(rowA);
         $.validateData(rowB);
@@ -116,29 +138,28 @@ $(document).ready(function()
                 "II": [1,2,3,4,5,6,7,8,9]
             };
             
-        console.log(markerBoard);
+        //console.log(markerBoard);
         
         //create puzzle board 9 x 9
-        var puzzleBoard = new Array(9); //create an array allocated with 9 spots
-        for(i = 0; i < puzzleBoard.length; i++) //iterate array and add an array object
+        var puzzleBoard = new Array(SUDOKU_BOARD_LENGTH); //create an array allocated with 9 spots
+        for(i = 0; i < SUDOKU_BOARD_LENGTH; i++) //iterate array and add an array object
         {
-             puzzleBoard[i] = new Array(9); //add column to puzzle board    
+             puzzleBoard[i] = new Array(SUDOKU_BOARD_LENGTH); //add column to puzzle board    
         }
         
         //Populate puzzle board with given values
-        for(i = 0; i < givenValues.length; i++)//Iterate each row
+        for(i = 0; i < SUDOKU_BOARD_LENGTH; i++)//Iterate each row
         {
             var str = givenValues[i];
-            
+
             //iterate each char in row
-            for(j=0; j< str.length; j++)
+            for(j=0; j< SUDOKU_BOARD_LENGTH; j++)
             {
                 puzzleBoard[j][i] = Number(str.charAt(j)); //add number into puzzle board 
             }
         }
-        
         console.log(puzzleBoard);
-       
+        console.log($.validateSudokuConstraint(puzzleBoard)); //check if current board is valid
     });
 });
 
@@ -147,7 +168,7 @@ $(document).ready(function()
     /*
      * Validate the data of user input for a row.
      * Length of row must be nine. Only accept numbers. 
-     * @param - rowArray An array of characters [String]
+     * @param - rowArray [String] An array of characters 
      * @return - true if validation passes; false otherwise
      */
     $.validateData = function(rowArray)
@@ -156,5 +177,123 @@ $(document).ready(function()
         return (rowArray.match(digitRegex) == null)? false : true;    
     }
     
+    /*
+     * Validate if the sudoku constraints are satisfied
+     * Zero represents a blank.
+     * 1) Each row must not contain duplicate numbers [1 -9].
+     * 2) Each column must not contain duplicate numbers [1-9].
+     * 3) Each quadrant must not contain duplicate numbers.
+     * @param - puzzleBoard [2D Array] the current state of the sudoku board
+     * @return - true if validation passes; false otherwise
+     */
+    $.validateSudokuConstraint = function(puzzleBoard)
+    {
+        var arrayNumberCounter = [0,0,0,0,0,0,0,0,0,0]; //counts the numbers used per row, column, and quadrant
+        
+        //Validate rows
+        for(var i = 0 ; i < SUDOKU_BOARD_LENGTH; i++)
+        {
+            $.calculateRowCounters(puzzleBoard, arrayNumberCounter, i); //calculate counters       
+            if(!$.uniquenessConstraint(arrayNumberCounter)){return false;} //validate number uniqueness constraint
+            $.resetCounterArray(arrayNumberCounter); //reset number counter array    
+        }
+
+        //Validate columns
+        for(var j = 0 ; j < SUDOKU_BOARD_LENGTH; j++)
+        {
+            $.calculateColumnCounters(puzzleBoard, arrayNumberCounter, j); //calculate counters       
+            if(!$.uniquenessConstraint(arrayNumberCounter)){return false;} //validate number uniqueness constraint
+            $.resetCounterArray(arrayNumberCounter); //reset number counter array
+        }     
+        
+        //Validate quadrants
+        for(var k = 0 ; k < SUDOKU_BOARD_LENGTH; k++)
+        {
+            $.calculateQuadrantCounters(puzzleBoard, arrayNumberCounter, k);
+            if(!$.uniquenessConstraint(arrayNumberCounter)){return false;} //validate number uniqueness constraint
+            $.resetCounterArray(arrayNumberCounter); //reset number counter array
+        }
+
+        return true; //at this point all validation pass 
+    }
+    
+    /*
+     * Calculate counter array for a row.
+     * @param - puzzleBoard [2D Array] current state of sudoku puzzle board
+     * @param - rowCounterArray [Array] increment value for corresponding index (use tile value)
+     * @param - selectedRow [int] the selected row to look at (A = 0, B =1, ... , I = 8 )
+     */
+    $.calculateRowCounters = function(puzzleBoard, rowCounterArray, selectedRow)
+    {
+        for(var i = 0; i < SUDOKU_BOARD_LENGTH; i++)
+        {
+              rowCounterArray[puzzleBoard[i][selectedRow]]++;
+              //console.log("(" + i + "," + selectedRow + ")");
+        }
+    }
+    
+    /*
+     * Calculate counter array for a column.
+     * @param - puzzleBoard [2D Array] current state of sudoku puzzle board
+     * @param - columnCounterArray [Array] increment value for corresponding index (use tile value)
+     * @param - selectedColumn [int] the selected column to look at (A = 0, B =1, ... , I = 8 )
+     */
+    $.calculateColumnCounters = function(puzzleBoard, columnCounterArray, selectedColumn)
+    {
+        for(var j = 0; j < SUDOKU_BOARD_LENGTH; j++)
+        {
+              columnCounterArray[puzzleBoard[selectedColumn][j]]++;
+              //console.log("(" + selectedColumn  + "," + j + ")");
+        }
+    }
+    
+    /*
+     * Calculate counter array for a quadrant.
+     * @param - puzzleBoard [2D Array] current state of sudoku puzzle board
+     * @param - quadrantCounterArray [Array] increment value for corresponding index (use tile value)
+     * @param - selectedQuadrant [int] the selected quadrant to look coordinate system (I = 0, II = 1, ..., XI = 8)
+     */
+    $.calculateQuadrantCounters = function(puzzleBoard, quadrantCounterArray, selectedQuadrant)
+    {
+        var x = 0;
+        var y = 0;
+        
+        for(var k = 0; k < SUDOKU_BOARD_LENGTH; k++)
+        {
+            x = (k % 3) + (3 * (selectedQuadrant % 3)); 
+            y = Math.floor(k / 3) + (3 * Math.floor(selectedQuadrant / 3));
+            quadrantCounterArray[puzzleBoard[x][y]]++;
+            //console.log("(" + x + "," + y + ")");
+        }
+    }
+    
+    /*
+     * Validates sudoku uniqueness constraint.
+     * @param - arrayCounter [Array] containing number counters for a row, column, or quadrant
+     * @return - true if constraint passes; false otherwise
+     */
+    $.uniquenessConstraint = function(arrayCounter)
+    {
+        var arrayLength = arrayCounter.length;
+        for(var i = 1; i < arrayLength; i++ ) //start at 1 to skip 0 counter (blanks)
+        {
+            if(arrayCounter[i] > 1)
+                return false;
+        }
+        return true;
+    }
+    
+    /*
+     * Reset array counter to zeroes.
+     * @param - arrayCounter
+     */
+    $.resetCounterArray = function(arrayCounter)
+    {
+        var arrayLength = arrayCounter.length;
+        for(var i = 0; i < arrayLength ; i++)
+        {
+            arrayCounter[i] = 0;    
+        }
+    }
     
 })(jQuery);
