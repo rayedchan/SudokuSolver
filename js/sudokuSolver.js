641cc68e142097db3f405ca41b6eb8c22ab02859
rayedchan <rayedchan@hotmail.com>
2013-05-14 20:34:09 -0400

    started on marker eliminator for initial board

--- js/sudokuSolver.js
+++ js/sudokuSolver.js
@@ -53,93 +53,21 @@ $(document).ready(function()
         //Build marker board. The sudoku board is made of 81 coordinate points. 
         //Each coordinate point has it own corresponding marker list.
         //The marker list represents all the possible numbers that can be place in that coordinate.
-        var markerBoard = 
-            {
-                "AA": [1,2,3,4,5,6,7,8,9],
-                "AB": [1,2,3,4,5,6,7,8,9],
-                "AC": [1,2,3,4,5,6,7,8,9],
-                "AD": [1,2,3,4,5,6,7,8,9],
-                "AE": [1,2,3,4,5,6,7,8,9],
-                "AF": [1,2,3,4,5,6,7,8,9],
-                "AG": [1,2,3,4,5,6,7,8,9],
-                "AH": [1,2,3,4,5,6,7,8,9],
-                "AI": [1,2,3,4,5,6,7,8,9],
-                "BA": [1,2,3,4,5,6,7,8,9],
-                "BB": [1,2,3,4,5,6,7,8,9],
-                "BC": [1,2,3,4,5,6,7,8,9],
-                "BD": [1,2,3,4,5,6,7,8,9],
-                "BE": [1,2,3,4,5,6,7,8,9],
-                "BF": [1,2,3,4,5,6,7,8,9],
-                "BG": [1,2,3,4,5,6,7,8,9],
-                "BH": [1,2,3,4,5,6,7,8,9],
-                "BI": [1,2,3,4,5,6,7,8,9],
-                "CA": [1,2,3,4,5,6,7,8,9],
-                "CB": [1,2,3,4,5,6,7,8,9],
-                "CC": [1,2,3,4,5,6,7,8,9],
-                "CD": [1,2,3,4,5,6,7,8,9],
-                "CE": [1,2,3,4,5,6,7,8,9],
-                "CF": [1,2,3,4,5,6,7,8,9],
-                "CG": [1,2,3,4,5,6,7,8,9],
-                "CH": [1,2,3,4,5,6,7,8,9],
-                "CI": [1,2,3,4,5,6,7,8,9],
-                "DA": [1,2,3,4,5,6,7,8,9],
-                "DB": [1,2,3,4,5,6,7,8,9],
-                "DC": [1,2,3,4,5,6,7,8,9],
-                "DD": [1,2,3,4,5,6,7,8,9],
-                "DE": [1,2,3,4,5,6,7,8,9],
-                "DF": [1,2,3,4,5,6,7,8,9],
-                "DG": [1,2,3,4,5,6,7,8,9],
-                "DH": [1,2,3,4,5,6,7,8,9],
-                "DI": [1,2,3,4,5,6,7,8,9],
-                "EA": [1,2,3,4,5,6,7,8,9],
-                "EB": [1,2,3,4,5,6,7,8,9],
-                "EC": [1,2,3,4,5,6,7,8,9],
-                "ED": [1,2,3,4,5,6,7,8,9],
-                "EE": [1,2,3,4,5,6,7,8,9],
-                "EF": [1,2,3,4,5,6,7,8,9],
-                "EG": [1,2,3,4,5,6,7,8,9],
-                "EH": [1,2,3,4,5,6,7,8,9],
-                "EI": [1,2,3,4,5,6,7,8,9],
-                "FA": [1,2,3,4,5,6,7,8,9],
-                "FB": [1,2,3,4,5,6,7,8,9],
-                "FC": [1,2,3,4,5,6,7,8,9],
-                "FD": [1,2,3,4,5,6,7,8,9],
-                "FE": [1,2,3,4,5,6,7,8,9],
-                "FF": [1,2,3,4,5,6,7,8,9],
-                "FG": [1,2,3,4,5,6,7,8,9],
-                "FH": [1,2,3,4,5,6,7,8,9],
-                "FI": [1,2,3,4,5,6,7,8,9],
-                "GA": [1,2,3,4,5,6,7,8,9],
-                "GB": [1,2,3,4,5,6,7,8,9],
-                "GC": [1,2,3,4,5,6,7,8,9],
-                "GD": [1,2,3,4,5,6,7,8,9],
-                "GE": [1,2,3,4,5,6,7,8,9],
-                "GF": [1,2,3,4,5,6,7,8,9],
-                "GG": [1,2,3,4,5,6,7,8,9],
-                "GH": [1,2,3,4,5,6,7,8,9],
-                "GI": [1,2,3,4,5,6,7,8,9],
-                "HA": [1,2,3,4,5,6,7,8,9],
-                "HB": [1,2,3,4,5,6,7,8,9],
-                "HC": [1,2,3,4,5,6,7,8,9],
-                "HD": [1,2,3,4,5,6,7,8,9],
-                "HE": [1,2,3,4,5,6,7,8,9],
-                "HF": [1,2,3,4,5,6,7,8,9],
-                "HG": [1,2,3,4,5,6,7,8,9],
-                "HH": [1,2,3,4,5,6,7,8,9],
-                "HI": [1,2,3,4,5,6,7,8,9],
-                "IA": [1,2,3,4,5,6,7,8,9],
-                "IB": [1,2,3,4,5,6,7,8,9],
-                "IC": [1,2,3,4,5,6,7,8,9],
-                "ID": [1,2,3,4,5,6,7,8,9],
-                "IE": [1,2,3,4,5,6,7,8,9],
-                "IF": [1,2,3,4,5,6,7,8,9],
-                "IG": [1,2,3,4,5,6,7,8,9],
-                "IH": [1,2,3,4,5,6,7,8,9],
-                "II": [1,2,3,4,5,6,7,8,9]
-            };
+        //Multi-dimensional array 9x9x9 cube 
+        var markerBoard = new Array(SUDOKU_BOARD_LENGTH);
+        var quadrantBoard = new Array(SUDOKU_BOARD_LENGTH); //corresponding coordinate to quadrant
+        for(i = 0; i < SUDOKU_BOARD_LENGTH; i++)
+        {
+            markerBoard[i] = new Array(SUDOKU_BOARD_LENGTH);
+            quadrantBoard[i] = new Array(SUDOKU_BOARD_LENGTH);
             
+            for(j =0; j < SUDOKU_BOARD_LENGTH; j++)
+            {
+                markerBoard[i][j] = [1,2,3,4,5,6,7,8,9]; 
+            }    
+        }
         //console.log(markerBoard);
-        
+
         //create puzzle board 9 x 9
         var puzzleBoard = new Array(SUDOKU_BOARD_LENGTH); //create an array allocated with 9 spots
         for(i = 0; i < SUDOKU_BOARD_LENGTH; i++) //iterate array and add an array object
@@ -155,11 +83,48 @@ $(document).ready(function()
             //iterate each char in row
             for(j=0; j< SUDOKU_BOARD_LENGTH; j++)
             {
-                puzzleBoard[j][i] = Number(str.charAt(j)); //add number into puzzle board 
+                puzzleBoard[j][i] = Number(str.charAt(j)); //add number into puzzle board
+                x = (j % 3) + (3 * (i % 3)); 
+                y = Math.floor(j / 3) + (3 * Math.floor(i / 3));
+                quadrantBoard[x][y] = i; //populate quadrant board
             }
         }
-        console.log(puzzleBoard);
-        console.log($.validateSudokuConstraint(puzzleBoard)); //check if current board is valid
+        //console.log(quadrantBoard);
+        //console.log(puzzleBoard);
+        //console.log($.validateSudokuConstraint(puzzleBoard)); //check if current board is valid
+        
+        var continuousLoop = true;
+        while(continuousLoop)
+        {
+            //Iterate entire puzzle board
+            for(i = 0; i < SUDOKU_BOARD_LENGTH; i++)
+            {
+                for(j = 0; j < SUDOKU_BOARD_LENGTH; j++)
+                {
+                    var coordinateValue = puzzleBoard[i][j];
+                    var coordinateQuadrant = quadrantBoard[i][j]; 
+                   
+                    if(coordinateValue != 0)
+                    {
+                        for(k = 0; k < SUDOKU_BOARD_LENGTH; k++)
+                        {
+                            markerBoard[k][j][coordinateValue - 1] = 0; //remove markers from row
+                            markerBoard[i][k][coordinateValue - 1] = 0; //remove markers from column
+                            x = (k % 3) + (3 * (coordinateQuadrant % 3)); 
+                            y = Math.floor(k / 3) + (3 * Math.floor(coordinateQuadrant / 3));
+                            markerBoard[x][y][coordinateValue - 1] = 0; //remove markers from quadrant
+                            //console.log(k + ", " + j);
+                            //console.log(i + ", " + k);
+                            //console.log(x + ", " + y);
+                        }
+                    }
+                }
+            }
+            
+            break;
+        }
+       
+        //console.log(markerBoard);
     });
 });
 
@@ -185,6 +150,7 @@ $(document).ready(function()
      * 3) Each quadrant must not contain duplicate numbers.
      * @param - puzzleBoard [2D Array] the current state of the sudoku board
      * @return - true if validation passes; false otherwise
+     * TODO: Merge for loops into one loop
      */
     $.validateSudokuConstraint = function(puzzleBoard)
     {
